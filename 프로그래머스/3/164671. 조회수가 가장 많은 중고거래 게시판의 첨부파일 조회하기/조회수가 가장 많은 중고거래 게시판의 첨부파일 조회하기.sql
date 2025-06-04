35d08f343de72211743fd51b9652271b7ac0609d
heebeom-song <55120757+heebeom-song@users.noreply.github.com>
2025-06-04 20:39:21 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,17 @@
+-- 코드를 입력하세요
+SELECT
+    concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) AS file_path
+FROM
+    USED_GOODS_FILE
+WHERE
+    board_id = (
+        SELECT
+            board_id
+        FROM
+            USED_GOODS_BOARD
+        ORDER BY
+            views DESC
+        LIMIT 1
+    )
+ORDER BY
+    file_id DESC;
